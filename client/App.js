6f40069a837fc6e43eeca8758639c854fe76abc6
Yannis Tarfa <yannistarfa98@gmail.com>
2019-01-26 10:46:23 -0500

    Header set-up and styled.

--- client/App.js
+++ client/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
 import Home from './Home';
+import { createGlobalStyle } from 'styled-components'
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-size: 62.5%;
+    h1 {
+        font-family: Roboto, sans-serif;
+        font-weight: 300;
+        line-height: 1.8;
+        font-size: 3em;
+    }
+  }
+`
 export default class App extends React.Component {
     render() {
         return (
             <div>
+                <GlobalStyle />
                 <Home />
             </div>
         )
--- client/Home.js
+++ client/Home.js
@@ -7,14 +7,40 @@ const StyledHome = styled.div`
 
 const StyledHeader = styled.div`
     position: relative;
-    padding: 50px;
+    padding: 50px 50px 300px 50px;
     box-sizing: border-box;
-    height: 500px;
     max-width: 1300px;
     margin: auto;
+
+    h1 {
+        color: white;
+        margin-left: 175px;
+        margin-top: 0;
+    }
+
+    .navigation-container {
+        position: relative;
+    }
+
+    .intro-container {
+        margin-top: 100px;
+    }
+`
+
+const MenuButton = styled.button`
+    position: absolute;
+    left: 0; top: 0;
+    border: none;
+    height: 50px; width: 50px;
+    -webkit-box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+    -moz-box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+    -ms-box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+    -o-box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
 `
 
 const StyledHeaderBackground = styled.svg`
+    z-index: -1;
     position: absolute;
     top: 0; left: 0;
     width: 100%; height: 100%;
@@ -25,6 +51,7 @@ export default class Home extends React.Component {
         return (
             <StyledHome>
                 <StyledHeader>
+
                     <StyledHeaderBackground viewBox="0 0 100 100" preserveAspectRatio="none">
                         <defs>
                             <linearGradient id="grad" x1='0%' y1='0%' x2='100%' y2='100%'>
@@ -34,6 +61,17 @@ export default class Home extends React.Component {
                         </defs>
                         <polygon fill='url(#grad)' points='0,0 100,0 100,100 0,80' />
                     </StyledHeaderBackground>
+
+                    <div class="header-content-wrapper">
+                        <div class="navigation-container">
+                            <MenuButton></MenuButton>
+                            <h1> Yannis Tarfa </h1>
+                        </div>
+
+                        <div class="intro-container">
+                            <h1> University Student and Web Developer based in Montreal, Canada. <br/> I like building stuff. </h1>
+                        </div>
+                    </div>
 
                 </StyledHeader>
             </StyledHome>
