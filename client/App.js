cd44eb02147a3979d81330373ab8efdd2fb6e58b
Yannis Tarfa <yannistarfa98@gmail.com>
2019-01-26 15:12:28 -0500

    Global style changes

--- client/App.js
+++ client/App.js
@@ -6,15 +6,29 @@ import { createGlobalStyle } from 'styled-components'
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
-    font-size: 62.5%;
+
     h1 {
+        font-family: Roboto, sans-serif;
+        font-weight: 400;
+        line-height: 1.8;
+    }
+
+    h2 {
         font-family: Roboto, sans-serif;
         font-weight: 300;
         line-height: 1.8;
-        font-size: 3em;
     }
+
+    p {
+        font-family: Roboto, sans-serif;
+        font-size: 20px;
+        color: #333333;
+        font-weight: 300;
+    }
+
   }
 `
+
 export default class App extends React.Component {
     render() {
         return (
