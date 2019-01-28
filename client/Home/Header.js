d8f016644261875a2e8866f560ff8a1e0e85437d
Yannis Tarfa <yannistarfa98@gmail.com>
2019-01-27 19:36:43 -0500

    Changes to header section.

--- client/Home/Header.js
+++ client/Home/Header.js
@@ -4,25 +4,62 @@ import Particles from 'react-particles-js';
 
 const Header = styled.div`
     position: relative;
-    padding: 50px 50px 300px 50px;
+    padding: 100px 50px 100px 50px;
     box-sizing: border-box;
     max-width: 100%;
     margin: auto;
     background-color: #F8F8F8;
 
-    h1 {
-        color: white;
-        max-width: 925px;
-        margin: auto;
-    }
-
     .header-content-wrapper {
 
         .navigation-container {
             position: relative;
+            width: 925px;
+            margin: auto;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+
+            h1 {
+                display: inline-block;
+                margin: 0;
+                color: white;
+            }
+
+            ul {
+                float: right;
+                margin: 0;
+                padding: 0;
+                li {
+                    display: inline-block;
+                    padding: 0 20px 0 20px;
+                    i {
+                        padding-right: 5px;
+                    }
+                    a {
+                        font-size: 15px;
+                        font-weight: 300;
+                        color: white;
+                        opacity: 0.7;
+                        transition: 0.25s;
+                        &:hover {
+                            opacity: 1;
+                        }
+                    }
+                    .email {
+                        border-bottom: solid 1px #FFFFFF;
+                    }
+                }
+            }
         }
     
         .intro-container {
+            h1 {
+                color: white;
+                max-width: 925px;
+                margin: auto;
+                text-align: center;
+            }
             position: relative;
             margin-top: 100px;
         }
@@ -62,12 +99,12 @@ export default class Home extends React.Component {
 
                 <HeaderBackground viewBox="0 0 100 100" preserveAspectRatio="none">
                     <defs>
-                        <linearGradient id="gradient" x1='0%' y1='0%' x2='100%' y2='100%'>
+                        <linearGradient id="gradient" x1='50%' y1='30%' x2='50%' y2='100%'>
                             <stop offset="20%" style={{ stopColor: '#3366DB', stopOpacity:1 }} />
                             <stop offset="100%" style={{ stopColor: '#6286DB', stopOpacity:1 }} />
                         </linearGradient>
                     </defs>
-                    <polygon fill="url(#gradient)" points='0,0 100,0 100,95 0,80' />
+                    <polygon fill="url(#gradient)" points='0,0 100,0 100,100 0,100' />
                 </HeaderBackground>
 
                 <ParticleWrapper>
@@ -80,12 +117,16 @@ export default class Home extends React.Component {
 
                 <div class="header-content-wrapper">
                     <div class="navigation-container">
-                        <MenuButton></MenuButton>
                         <h1> Yannis Tarfa </h1>
+                        <ul>
+                            <li><a href="https://www.linkedin.com/in/yannistarfa/" target="_blank"> <i class="fab fa-linkedin"></i> LinkedIn </a></li>
+                            <li><a href="https://github.com/YT98" target="_blank"> <i class="fab fa-github-square"></i> GitHub </a></li>
+                            <li><a href="mailto:info@yannistarfa.com" class="email"> info@yannistarfa.com </a></li>
+                        </ul>
                     </div>
 
                     <div class="intro-container">
-                        <h1> University Student and Web Developer based in Montreal, Canada. </h1>
+                        <h1> Hi, I'm Yannis. I'm a university student and web developer from Montreal, Canada. </h1>
                     </div>
                 </div>
 
