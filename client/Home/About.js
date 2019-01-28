836e9715e567a641a0b61b2ae6f4eefdeda911d0
Yannis Tarfa <yannistarfa98@gmail.com>
2019-01-28 05:15:24 -0500

    Added work/education items.

--- client/Home/About.js
+++ client/Home/About.js
@@ -61,6 +61,42 @@ export default class About extends React.Component {
 
         const WorkEducation = styled.div`
             padding-top: 100px;
+            .item {
+                padding-bottom: 25px;
+                .date-title {
+                    display: flex;
+                    align-items: center;
+                    p {
+                        font-weight: 400;
+                    }
+                    span {
+                        font-weight: 300;
+                        padding: 0 5px 0 5px;
+                    }
+                    .date {
+                        width: 250px;
+                    }
+                    .title {
+
+                    }
+                }
+                .description {
+                    ul {
+                        // list-style-type: none;
+                        padding: 0;
+                        margin-left: 265px;
+                        li {
+                            font-size: 10px;
+                            p {
+                                font-size: 15px;
+                                padding: 0;
+                                padding-left: 20px;
+                                margin-top: 10px;
+                            }
+                        }
+                    }
+                }
+            }
         `
 
         return (
@@ -133,7 +169,74 @@ export default class About extends React.Component {
 
                     <WorkEducation>
                         <h1> Education </h1>
+                        <div class="item">
+                            <div class="date-title">
+                                <p class="date"> 2018/09 - Present </p>
+                                <p class="title"> Université de Montréal </p>
+                            </div>
+                            <div class="description">
+                                <ul>
+                                    <li><p> Physics and Computer Science B.Sc. (starting Fall 2019) </p></li>
+                                    <li><p> Preparatory Year - Pure and Applied Sciences Specialization (Currently enrolled, GPA: 3.8) </p></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="date-title">
+                                <p class="date"> 2014 - 2015 </p>
+                                <p class="title"> Collège Jean-de-Brébeuf </p>
+                            </div>
+                            <div class="description">
+                                <ul>
+                                    <li><p> Humanities - International Studies (with Advanced Mathematics) </p></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="item" style={{ paddingBottom: '100px' }}>
+                            <div class="date-title">
+                                <p class="date"> 2009 - 2014 </p>
+                                <p class="title"> Collège International Marie de France </p>
+                            </div>
+                            <div class="description">
+                                <ul>
+                                    <li><p> DES (Diplôme d'Études Secondaires) </p></li>
+                                    <li><p> ES (Economic and Social Sciences program) </p></li>
+                                </ul>
+                            </div>
+                        </div>
+
                         <h1> Work experience</h1>
+                        <div class="item">
+                            <div class="date-title">
+                                <p class="date"> 01/2017 - Present </p>
+                                <p class="title"> Freelance Web Developer </p>
+                            </div>
+                            <div class="description">
+                                <ul>
+                                    <li><p> Full Stack Web Development (NGINX, Node.js, Express, MongoDB, React.js, HTML, CSS). </p></li>
+                                    <li><p> My role also included client prospection, defining the client's needs, preparing the work scope as well as client follow-up. </p></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="item" style={{ paddingBottom: '100px' }}>
+                            <div class="date-title">
+                                <p class="date"> 2018/01 - 2018/05 </p>
+                                <p class="title"> Junior Web Developer <span>at</span> Empty Cubicle </p>
+                            </div>
+                            <div class="description">
+                                <ul>
+                                    <li><p> Full Stack Web Development (ASP.NET MVC, Azure, MicrosoftSQLServer, React.js, Redux.js, HTML, CSS). </p></li>
+                                    <li><p> Following an AGILE development approach. </p></li>
+                                    <li><p> Development of a smart resume verification system alongside artificial intelligence consultant agencies. </p></li>
+                                    <li><p> Worked on a number of essential features for the application, such as user registration and account preferences. </p></li>
+                                </ul>
+                                <ul>
+                                    <li><p> Put in charge of implementing React.js (coupled with Redux) as a project-wide front-end framework. Trained developers on the team on how to properly implement these technologies in their work flow. </p></li>
+                                    <li><p> Implemented REST API architectural pattern in a previously MVC-based (.cshtml Razor pages) project. </p></li>
+                                </ul>
+                            </div>
+                        </div>
+
                     </WorkEducation>
 
                 </div>
