0deec4bd9cd282f54ab69ba56c8392c38267a17b
Yannis Tarfa <yannistarfa98@gmail.com>
2019-01-28 07:54:05 -0500

    Moved images folder.

--- client/Home/Work.js
+++ client/Home/Work.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import {holidayHomesImage} from '../../images/index';
-import {condosFabreImage} from '../../images/index'
+import {holidayHomesImage} from '../images/index';
+import {condosFabreImage} from '../images/index'
 import deviceSizes from '../deviceSizes';
 
 const WorkWrapper = styled.div`
