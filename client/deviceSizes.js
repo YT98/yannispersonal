5e312de0b443aab60ac9ce858be1629a39ed452f
Yannis Tarfa <yannistarfa98@gmail.com>
2019-01-28 07:28:45 -0500

    Device sizes change.

--- client/deviceSizes.js
+++ client/deviceSizes.js
@@ -2,17 +2,12 @@ const sizes = {
     mobileS: '320px',
     mobileM: '375px',
     mobileL: '425px',
-    tablet: '720px',
-    laptop: '975px',
+    tablet: '975px'
 }
 
 export default {
-    mobileS: `(min-width: ${sizes.mobileS})`,
-    mobileM: `(min-width: ${sizes.mobileM})`,
-    mobileL: `(min-width: ${sizes.mobileL})`,
-    tablet: `(min-width: ${sizes.tablet})`,
-    laptop: `(min-width: ${sizes.laptop})`,
-    laptopL: `(min-width: ${sizes.laptopL})`,
-    desktop: `(min-width: ${sizes.desktop})`,
-    desktopL: `(min-width: ${sizes.desktop})`
+    mobileS: `(max-width: ${sizes.mobileS})`,
+    mobileM: `(max-width: ${sizes.mobileM})`,
+    mobileL: `(max-width: ${sizes.mobileL})`,
+    tablet: `(max-width: ${sizes.tablet})`
 };
